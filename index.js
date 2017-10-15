eee7f70187b165811b17f40bd30e8f10b59a7291
Jose Ruiz <sam92.sp@gmail.com>
2017-10-15 11:36:57 +0200

    Transpile and Uglify public js added

--- index.js
+++ index.js
@@ -2,16 +2,18 @@ let express = require('express');
 let app = express();
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
-let port = process.env.PORT || 3000;
 let server = require('./server/server');
 
 // Client
-// Serve contents of "client" folder on localhost
-app.use(express.static('client'));
+// Serve contents of "client" or "dist" folder
+let client_path = (typeof process.env.DIST !== 'undefined') ? 'dist' : 'client';
+console.log('Client path: ' + client_path);
+app.use(express.static(client_path));
 
 // Server
 server.init(io);
 
+let port = process.env.PORT || 3000;
 http.listen(port, () => {
     console.log('Listening on port: ' + port);
 });
