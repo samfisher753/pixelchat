73c34bcadf97e14f425a2a94d5e36438ddd9827f
Jose Ruiz <sam92.sp@gmail.com>
2024-10-10 18:56:21 +0200

    Removed some files

--- bin/deploy.bat
+++
@@ -1,6 +0,0 @@
-setlocal
-cd ..
-docker build -t samfisher753/pixelchat:latest .
-docker push samfisher753/pixelchat:latest
-ssh opc@143.47.44.201 "sudo docker stop pixelchat; sudo docker rm pixelchat; sudo docker pull samfisher753/pixelchat:latest; sudo docker run -d -p 3000:3000 --name pixelchat samfisher753/pixelchat:latest"
-endlocal
