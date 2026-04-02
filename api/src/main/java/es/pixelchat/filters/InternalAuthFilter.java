d6f95f83cdf7fea79c7a4390d43dd86766ff8a03
Jose Ruiz <sam92.sp@gmail.com>
2026-04-03 01:30:35 +0200

    Fix some bugs in api

--- api/src/main/java/es/pixelchat/filters/InternalAuthFilter.java
+++ api/src/main/java/es/pixelchat/filters/InternalAuthFilter.java
@@ -1,23 +1,22 @@
 package es.pixelchat.filters;
 
-import io.quarkus.runtime.annotations.StaticInitSafe;
+import jakarta.inject.Singleton;
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerRequestFilter;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.Provider;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.config.ConfigProvider;
 
+@Singleton
 @Provider
 @InternalOnly
 public class InternalAuthFilter implements ContainerRequestFilter {
 
-    /** En imagen nativa el secreto debe venir del entorno en runtime, no del build. */
-    @StaticInitSafe
-    @ConfigProperty(name = "internal.secret")
-    String secret;
-
     @Override
     public void filter(ContainerRequestContext ctx) {
+        // Leído en runtime puro: ConfigProvider nunca se evalúa en el static init
+        // del binario nativo, evitando que el valor quede congelado en build-time.
+        String secret = ConfigProvider.getConfig().getValue("internal.secret", String.class);
         String header = ctx.getHeaderString("X-Internal-Secret");
         if (!secret.equals(header)) {
             ctx.abortWith(Response.status(Response.Status.FORBIDDEN).build());
